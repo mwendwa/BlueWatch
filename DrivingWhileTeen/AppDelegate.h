68ce6d7b2ff7f783f486d095e8626536d87021cc
Eugene Alute Mwendwa <mwendwa@Denees-MBP-2.lan>
2014-11-14 14:40:21 -0500

    Initial Commit

---
+++ DrivingWhileTeen/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DrivingWhileTeen
+//
+//  Created by Eugene Alute Mwendwa on 11/14/14.
+//  Copyright (c) 2014 SafeNet Industries. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
